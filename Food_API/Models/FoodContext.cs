48b3fb4d30c18c66ba145fb786b80aa26049cd6e
Vasjana Spaho <vasjanaspaho@outlook.fr>
2022-01-29 16:59:37 +0100

    Add project files.

---
+++ Food_API/Models/FoodContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Food_API.Models
+{
+    public class FoodContext : DbContext
+    {
+       
+        public FoodContext(DbContextOptions<FoodContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<FoodItem> FoodItems { get; set; }
+    }
+}
---
+++ Food_API/Models/FoodItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Food_API.Models
+{
+    public class FoodItem
+    {
+        public long Id { get; set; }
+        public int Numbers {get; set; }
+        public string Tags { get; set; }
+        public bool LimitLicense { get; set; }
+    }
+}
